bc83d190f77efec7cd23a0278e6b9cae6de796d2
Inna <inna.kornieko@gmail.com>
2022-11-10 19:37:11 +0200

    Upd file

--- src/components/Phonebook/PhonebookAdd.jsx
+++ src/components/Phonebook/PhonebookAdd.jsx
@@ -34,6 +34,22 @@ const PhonebookAdd = () => {
         return result;
     }
 
+   
+    const handleChange = e => {
+        switch (e.currentTarget.name) {
+          case 'name':
+            setName(e.currentTarget.value);
+            break;
+    
+          case 'number':
+            setNumber(e.currentTarget.value);
+            break;
+    
+          default:
+            return;
+        }
+      };
+
       const handleSubmit = (e) => {
         e.preventDefault();
         onAddContact({name, number})
@@ -48,7 +64,7 @@ const PhonebookAdd = () => {
             type="text"
             name="name"
             value={name}
-            onChange={onAddContact}
+            onChange={handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             placeholder="Please add name"
@@ -62,7 +78,7 @@ const PhonebookAdd = () => {
             type="tel"
             name="number"
             value={number}
-            onChange={onAddContact}
+            onChange={handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             placeholder="Please add telnumber"
